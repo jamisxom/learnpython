6bb00398d880afe2980d7895841ed7e6b5941630
JAMIS XOM <nil@nil.com>
2014-11-24 00:02:40 +0000

    final

--- ex32.py
+++ ex32.py
@@ -17,5 +17,11 @@ for i in range (0,6):
 	print "Adding %d to the list."  % i
 	elements.append(i)
 
+
+for i in elements:
+	print "Element was: %d" % i
+
+elements = range(0,6)
+
 for i in elements:
 	print "Element was: %d" % i
