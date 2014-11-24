0ae5426ad984505f893de0e6bf9b87ff71662c76
JAMIS XOM <nil@nil.com>
2014-11-24 02:51:21 +0000

    last commit

--- ex35.py
+++ ex35.py
@@ -15,4 +15,58 @@ def gold_room():
 	else:
 		dead("You greedy chap!")
 
+def bear_room():
+    print """there is a bear here
+            The bear has a bunch of honey.\n
+            The fat bear is in front of another door.\n
+            How are you going to move the bear?"""
+    bear_moved = False
 
+    while True:
+        choice = raw_input("> ")
+
+        if choice == "take honey":
+            dead("The bear looks at you then slaps your face off.")
+        elif choice =="taunt bear" and not bear_moved:
+            print "The bear has moved from the door. You can go through it"
+            bear_moved = True
+        elif choice =="taunt bear" and bear_moved:
+            dead("The bear gets pissed off and chews your leg off.")
+        elif choice == "open door" and bear_moved:
+            gold_room()
+        else:
+            print "I've got no idea what that means"
+
+def cthulu_room():
+    print """Here you see the great evil Cthulu.\n
+            He, it, whatever stares at you and you go insane\n
+            Do you flee for your life or eat your head?"""
+
+    choice = raw_input("> ")
+
+    if "flee" in choice:
+        start()
+    elif "head" in choice:
+        dead("Well that was tasty")
+    else:
+        cthulu_room()
+
+def dead(why):
+    print why, "Good job!"
+    exit(0)
+
+def start():
+    print """You are in a dark room.\n
+            There is a door to your right and left.\n
+            Which one do you take?"""
+
+    choice = raw_input("> ")
+
+    if choice == "left":
+        bear_room()
+    elif choice == "right":
+        cthulu_room()
+    else:
+        dead("You stumbel around till you starve")
+
+    start()
